99c07753f9d8fcd039bf1e163d8fef95bab8b5fb
KrishnaMoorthy <krishnamoorthy@KrishnaMoorthys-MacBook-Pro.local>
2019-02-21 17:30:15 +0530

    Added browsers ion switch with capabilities in helper

--- src/test/java/helpers/BrowserHelper.java
+++ src/test/java/helpers/BrowserHelper.java
@@ -1,4 +1,37 @@
 package helpers;
 
+import enums.Browser;
+import org.openqa.selenium.WebDriverException;
+import org.openqa.selenium.remote.DesiredCapabilities;
+import utils.Runner;
+
 public class BrowserHelper {
+
+    static Browser browser = Browser.valueOf(Runner.config.get("browser"));
+
+    static String browserVersion = Runner.config.get("browserVersion");
+
+    public static DesiredCapabilities selectBrowser(DesiredCapabilities cap) {
+        switch (browser) {
+            case Chrome:
+                cap.setCapability("browserName", "chrome");
+                break;
+            case Firefox:
+                cap.setCapability("browserName", "firefox");
+                break;
+            case IE:
+                cap.setCapability("browserName", "internet explorer");
+                break;
+            case Opera:
+                cap.setCapability("browserName", "opera");
+            case Safari:
+                cap.setCapability("browserName", "safari");
+                break;
+            default:
+                throw new WebDriverException("No browser specified");
+            }
+        cap.setCapability("version", browserVersion);
+        return cap;
+
+    }
 }
