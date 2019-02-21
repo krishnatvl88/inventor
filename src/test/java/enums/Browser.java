2ac50392a41c1cc52f12f48e8483aba8c37b600d
KrishnaMoorthy <krishnamoorthy@KrishnaMoorthys-MacBook-Pro.local>
2019-02-21 12:21:54 +0530

    Create enum classes for platform, browser supported

---
+++ src/test/java/enums/Browser.java
@@ -0,0 +1,10 @@
+package enums;
+
+public enum Browser {
+    IE,
+    Edge,
+    Safari,
+    Firefox,
+    Chrome,
+    Opera
+}
---
+++ src/test/java/enums/Platform.java
@@ -0,0 +1,7 @@
+package enums;
+
+public enum Platform {
+        Windows,
+        OSX,
+        Ubuntu,
+}
