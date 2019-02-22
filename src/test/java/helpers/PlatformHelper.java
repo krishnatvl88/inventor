9f2f7fcbfee7f9fa9e4c8bcd887d6f58bc79f68b
KrishnaMoorthy <krishnamoorthy@KrishnaMoorthys-MacBook-Pro.local>
2019-02-22 10:10:32 +0530

    Added platform helper

--- src/test/java/helpers/PlatformHelper.java
+++ src/test/java/helpers/PlatformHelper.java
@@ -1,4 +1,34 @@
 package helpers;
 
+import enums.Platform;
+import org.openqa.selenium.WebDriverException;
+import org.openqa.selenium.remote.DesiredCapabilities;
+import utils.Runner;
+
 public class PlatformHelper {
+    static Platform platform = Platform.valueOf(Runner.config.get("platform"));
+
+    static String platformVersion = Runner.config.get("platformVersion");
+
+    public static DesiredCapabilities selectPlatform(DesiredCapabilities cap) {
+        switch (platform) {
+            case Windows:
+                cap.setCapability("os", "Windows");
+                cap.setPlatform(org.openqa.selenium.Platform.WINDOWS);
+                break;
+            case OSX:
+                cap.setCapability("platform", "OS X");
+                cap.setPlatform(org.openqa.selenium.Platform.MAC);
+                break;
+            case Ubuntu:
+                cap.setCapability("os", "Ubuntu");
+                cap.setPlatform(org.openqa.selenium.Platform.LINUX);
+                break;
+            default:
+                throw new WebDriverException("No platform found");
+        }
+        cap.setCapability("os_version", platformVersion);
+
+        return cap;
+    }
 }
